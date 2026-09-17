f5244c073091001c2b512a5925a0222bb3b055be
RyuTV <150388527+RyuTV@users.noreply.github.com>
2026-09-17 18:27:53 +0200

    Usa la distribución de la rama durante las pruebas

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/RyuTV/AeldoriaRPG/master/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/RyuTV/AeldoriaRPG/codex/configurar-launcher-aeldoria/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
